138e195a994851954923f36d8e4b9b69650f3d95
Hong Daepyo <h1o1n1g1@nate.com>
2021-08-01 23:17:38 +0900

    feat: movie average tdd exercise

---
+++ movie/app/src/main/java/movie/App.java
@@ -0,0 +1,7 @@
+package movie;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ movie/app/src/test/java/movie/MovieTest.java
@@ -0,0 +1,35 @@
+package movie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MovieTest {
+
+    private Movie movie;
+
+    @BeforeEach
+    void setUp() {
+        movie = new Movie();
+    }
+
+    @Test
+    void should_return_0_when_just_created() {
+        assertThat(movie.averageRating()).isEqualTo(0);
+    }
+
+    @Test
+    public void should_return_1_when_1_was_rated() throws Exception {
+        movie.rate(1);
+        assertThat(movie.averageRating()).isEqualTo(1);
+    }
+
+    @Test
+    public void should_return_4_when_3_and_5_were_rated() throws Exception {
+        movie.rate(3);
+        movie.rate(5);
+        assertThat(movie.averageRating()).isEqualTo(4);
+    }
+
+}
